fe10157c36b7ee36efb7264e6c11fb829b9e77aa
mjpark03 <mjpark03@gmail.com>
2017-05-12 14:50:45 +0900

    Move import expression to the top

--- src/main/scala/chat/Client.scala
+++ src/main/scala/chat/Client.scala
@@ -1,13 +1,13 @@
 package chat
 
-/**
-  * Created by Rachel on 2017. 5. 12..
-  */
-
 import akka.actor.{ Actor, ActorRef, Props }
 import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
+
+/**
+  * Created by Rachel on 2017. 5. 12..
+  */
 
 object Client {
 
