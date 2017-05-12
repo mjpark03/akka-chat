2ce618a30b12d6e8f6b4148012c3472f56db9310
mjpark03 <mjpark03@gmail.com>
2017-05-12 14:43:16 +0900

    Add Client class template

---
+++ src/main/scala/chat/Client.scala
@@ -0,0 +1,29 @@
+package chat
+
+/**
+  * Created by Rachel on 2017. 5. 12..
+  */
+
+import akka.actor.{ Actor, ActorRef, Props }
+import akka.io.{ IO, Tcp }
+import akka.util.ByteString
+import java.net.InetSocketAddress
+
+object Client {
+
+}
+
+class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor {
+
+  import Tcp._
+  import context.system
+
+  IO(Tcp) ! Connect(remote)
+
+  def receive = {
+
+    case _ =>
+
+  }
+
+}
