49eb8bb4f37297e5ecb0feb2812c8585b04cf0e7
mjpark03 <mjpark03@gmail.com>
2017-05-12 15:08:15 +0900

    Add receive messages for Client

--- src/main/scala/chat/Client.scala
+++ src/main/scala/chat/Client.scala
@@ -22,8 +22,32 @@ class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor {
 
   def receive = {
 
-    case _ =>
+    case CommandFailed(_: Connect) =>
+      listener ! "connect failed"
+      context stop self
 
+    case c @ Connected(remote, local) =>
+      listener ! c
+      val connection = sender()
+      connection ! Register(self)
+
+      context become {
+        case data: ByteString =>
+          connection ! Write(data)
+
+        case CommandFailed(w: Write) =>
+          listener ! "write failed"
+
+        case Received(data) =>
+          listener ! data
+
+        case "close" =>
+          connection ! Close
+
+        case _: ConnectionClosed =>
+          listener ! "connection closed"
+          context stop self
+      }
   }
 
 }
